3c5109401a2ff37f2178819e8171539643336506
Kgahlego <krantho@gmail.com>
2013-10-24 09:07:23 +0200

    Create print.js

---
+++ print.js
@@ -0,0 +1,18 @@
+<script>
+
+$("#printBtn").click(function(){
+    printcontent($("#content").html());
+});
+
+function printcontent(content)
+{
+    var mywindow = window.open('', '', '');
+    mywindow.document.write('<html><title>Print</title><body>');
+    mywindow.document.write(content);
+    mywindow.document.write('</body></html>');
+    mywindow.document.close();
+    mywindow.print();
+    return true;
+}
+
+</script>
